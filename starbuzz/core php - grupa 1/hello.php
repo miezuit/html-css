41b25f36b4652a055e4b3bf9e032ea7990fa9059
Radu Gasler <miezuit@gmail.com>
2018-11-08 17:54:42 +0200

    Add files via upload

---
+++ starbuzz/core php - grupa 1/hello.php
@@ -0,0 +1,5 @@
+<?php
+  echo 'Introduceti numele: ';
+  $nume = readline();
+  echo "Hello, $nume".PHP_EOL;
+?>
---
+++ starbuzz/core php - grupa 1/suma.php
@@ -0,0 +1,8 @@
+<?php
+  echo 'Introduceti primul numar: ';
+  $a = readline();
+  echo 'Introduceti al doilea numar: ';
+  $b = readline();
+  $sum = $a + $b;
+  echo "Suma este: $sum".PHP_EOL;
+?>
